0d9b25c85141bc60800947fc656ef78560b4fcc7
st0rmed <gleblazarev967@gmail.com>
2024-12-07 17:46:29 +0300

    first_commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+import sqlite3
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
+from PyQt6 import uic
+
+import sys
+print(sys.version, sys.platform, sys.executable)
+
+class CoffeeApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Информация о кофе")
+        self.setGeometry(100, 100, 800, 600)
+        uic.loadUi('main.ui', self)
+
+        self.load_coffee_data()
+
+    def load_coffee_data(self):
+        conn = sqlite3.connect('coffee.sqlite')
+        cursor = conn.cursor()
+
+        rows = cursor.execute('''
+            SELECT
+                ID, Название_сорта, Степень_обжарки, Молотый_в_зернах, Описание_вкуса, Цена, Объем_упаковки 
+            FROM 
+                coffee''').fetchall()
+
+        conn.close()
+
+        self.tableWidget.setRowCount(len(rows))
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(
+            ["ID", "Название сорта", "Степень обжарки", "Молотый/в зернах", "Описание вкуса", "Цена", "Объем упаковки"])
+
+        for i, row in enumerate(rows):
+            for j, value in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(value)))
+
+        self.tableWidget.resizeColumnsToContents()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = CoffeeApp()
+    window.show()
+    sys.exit(app.exec())
